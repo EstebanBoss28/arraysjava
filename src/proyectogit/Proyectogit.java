7bed77475d347fa30074c0977d6180286ae22725
Esteban <estebanrosmartinez@gmail.com>
2021-02-01 11:07:59 +0100

    primera version de mi`proyecto

---
+++ src/proyectogit/Proyectogit.java
@@ -0,0 +1,10 @@
+
+package proyectogit;
+
+public class Proyectogit {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/proyectogit/main01.java
@@ -0,0 +1,21 @@
+
+package proyectogit;
+
+public class main01 {
+
+    public static void main(String[] args) {
+        
+        int [] edad = {15,45,34};
+        String[][] nombre = {{"Luis","Miguel"},{"Carlos","Juan"},{"Arturo", "Lucho"}};
+        
+        for (int i=0;i<nombre.length;i++){
+            for(int j=0;j<nombre[0].length;j++){
+                System.out.print(nombre[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+    
+}
